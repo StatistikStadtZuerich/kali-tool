0f0631d4e823b1362e43eb1cc3868dc0d728ef49
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2024-08-08 09:57:52 +0200

    fix excel download by adding packages and adjusting pathx

--- R/KALI-package.R
+++ R/KALI-package.R
@@ -3,9 +3,11 @@
 
 ## usethis namespace: start
 #' @import dplyr
+#' @import openxlsx
 #' @import reactable
 #' @import zuericssstyle
 #' @importFrom icons icon_set
+#' @importFrom readxl read_excel
 #' @importFrom stringr str_remove_all
 #' @importFrom tidyr gather
 ## usethis namespace: end
--- R/ssz_download_excel.R
+++ R/ssz_download_excel.R
@@ -1,12 +1,12 @@
 ssz_download_excel <- function(filteredData, file, nameVote){
-    
+
     # Data Paths
-    hauptPfad <- "www/Titelblatt.xlsx"
-    imagePfad <- "www/logo_stzh_stat_sw_pos_1.png"
-    
+    path_title_page <- "inst/app/www/Titelblatt.xlsx"
+    path_logo <- "inst/app/www/logo_stzh_stat_sw_pos_1.png"
+
     # Read Data
-    data <- read_excel(hauptPfad, sheet = 1)
-    
+    data <- read_excel(path_title_page, sheet = 1)
+
     # Manipulate Data
     # Data Sheet 1
     data <- data %>%
@@ -14,10 +14,10 @@ ssz_download_excel <- function(filteredData, file, nameVote){
         Date = ifelse(is.na(Date), NA, paste0(format(Sys.Date(), "%d"), ".", format(Sys.Date(), "%m"), ".", format(Sys.Date(), "%Y"))),
         Titel = ifelse(is.na(Titel), NA, paste0("Resultat der Gemeinderatswahlen für Ihre Auswahl: ", nameVote))
         )
-    
-    selected <- list(c("T_1", "Resultat der Gemeinderatswahlen für Ihre Auswahl:", paste(nameVote), " ", " ","Quelle: Statistik Stadt Zürich, Präsidialdepartement")) %>% 
+
+    selected <- list(c("T_1", "Resultat der Gemeinderatswahlen für Ihre Auswahl:", paste(nameVote), " ", " ","Quelle: Statistik Stadt Zürich, Präsidialdepartement")) %>%
       as.data.frame()
-      
+
     # Data Sheet 2
     # Styling
     sty <- createStyle(fgFill="#ffffff")
@@ -27,21 +27,21 @@ ssz_download_excel <- function(filteredData, file, nameVote){
     styDefinition <- createStyle(valign = "top",
                                  wrapText = TRUE)
     styTitle <- createStyle(fontName = "Arial Black")
-    
+
     # Create Workbook
     wb <- createWorkbook()
-    
+
     # Add Sheets
     addWorksheet(wb, sheetName = "Inhalt", gridLines = FALSE)
     addWorksheet(wb, sheetName = "T_1", gridLines = TRUE)
-    
+
     # Write Table Sheet 1
     writeData(wb, sheet = 1, x = data,
                  colNames = FALSE, rowNames = FALSE,
                  startCol = 2,
                  startRow = 7,
                  withFilter = FALSE)
-    
+
     # Write Table Sheet 2
     writeData(wb, sheet = 2, x = selected,
               colNames = FALSE, rowNames = FALSE,
@@ -53,16 +53,16 @@ ssz_download_excel <- function(filteredData, file, nameVote){
             startCol = 1,
             startRow = 9,
             withFilter = FALSE)
-    
+
     # Insert Logo on Sheet 1
-    insertImage(wb, imagePfad, sheet = 1, startRow= 2, startCol = 2, width = 1.75 , height = 0.35)
+    insertImage(wb, path_logo, sheet = 1, startRow= 2, startCol = 2, width = 1.75 , height = 0.35)
 
     # Add Styling
     addStyle(wb, 1, style = sty, row = 1:19, cols = 1:6, gridExpand = TRUE)
     addStyle(wb, 1, style = styTitle, row = 14, cols = 2, gridExpand = TRUE)
     addStyle(wb, 2, style = styConcept, row = 9, cols = 1:50, gridExpand = TRUE)
     modifyBaseFont(wb, fontSize = 8, fontName = "Arial")
-    
+
     # Set Column Width for content
     setColWidths(wb, sheet = 2, cols = "A", widths = 10)
     setColWidths(wb, sheet = 2, cols = "B", widths = 40)
@@ -73,13 +73,13 @@ ssz_download_excel <- function(filteredData, file, nameVote){
     setColWidths(wb, sheet = 2, cols = "G", widths = 40)
     setColWidths(wb, sheet = 2, cols = "H", widths = 35)
     setColWidths(wb, sheet = 2, cols = "I", widths = 12)
-    
+
     # Set Column Width for overview sheet
     setColWidths(wb, sheet = 1, cols = "A", widths = 1)
     setColWidths(wb, sheet = 1, cols = "B", widths = 4)
     setColWidths(wb, sheet = 1, cols = "D", widths = 40)
     setColWidths(wb, sheet = 1, cols = "E", widths = 10)
-    
+
     # Save Excel
     saveWorkbook(wb, file, overwrite = TRUE) ## save to working directory
 }
