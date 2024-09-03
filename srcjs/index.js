d2164bfb15286d2e41d05a671615cbea7bfb2b53
shaenzi <shaenzi@users.noreply.github.com>
2024-09-03 17:15:25 +0200

    add stylesheet in a hacky way

--- srcjs/index.js
+++ srcjs/index.js
@@ -1,15 +1,10 @@
 import * as d3 from 'd3';
 import * as sszvis from 'sszvis';
 import 'shiny';
-import { message } from './modules/message.js';
 import { makeBarChart } from './modules/makeBarChart.js';
 
-
-// In shiny server use:
-// session$sendCustomMessage('show-packer', 'hello packer!')
-Shiny.addCustomMessageHandler('show-packer', (msg) => {
-  message(msg);
-})
+// hacky way of adding the sszvis stylesheet (should also be possible based on the package?)
+$('head').append('<link rel="stylesheet" type="text/css" href="https://unpkg.com/sszvis@3/build/sszvis.css">');
 
 makeBarChart(d3, sszvis);
 
